628c52391ce719f0277c8b6ff68d5cb980ac6b98
liudongyun <liudongyun1215@sina.cn>
2015-01-15 08:21:43 +0800

    swiper制作的引导页

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,15 +8,21 @@ module.exports = function(grunt){
               paths: ["style/css"]
             },
             files: {
-              "style/main.css": "style/main.less"
+              "style/main.css": "style/main.less",
+              "style/css/index2.css":"style/less/index2.less"
             }
           }
+        },
+        watch:{
+          files:'style/**/*.less',
+          tasks:['less']
         }
     });
 
     // 加载提供任务的插件
     
     grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
     // 默认任务
     grunt.registerTask('default', ['less']);
